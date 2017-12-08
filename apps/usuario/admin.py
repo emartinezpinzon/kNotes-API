fa302322a0a6fa8064fe17bb248d7b2db595be40
Emanuel Martínez <emartinezpinzon@gmail.com>
2017-12-08 17:46:40 -0500

    Agrego el modelo autor y lo registro en admin

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from apps.usuario.models import Autor
+
+admin.site.register(Autor)
--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Autor(models.Model):
+    nombre=models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.nombre
