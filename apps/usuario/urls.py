b3de01f17bd39b2c2e7b691602c1c5c6776ee171
Emanuel Martínez <emartinezpinzon@gmail.com>
2017-12-08 21:53:46 -0500

    Registro de autores

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from apps.usuario import views
 
 urlpatterns = [
-
+    url(r'^registro/api/$', views.RegistroAutorList.as_view()),
 ]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,1 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from apps.usuario.models import Autor
+
+class RegistroAutorList(APIView):
+    def post(self,request,format=None):
+        nombre = request.data['nombre']
+        email = request.data['email']
+        password = request.data['password']
+
+        if User.objects.filter(username=email).exists():
+            return HttpResponse("Existe")
+        else:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+
+            autor=Autor(nombre=nombre, user=user)
+            autor.save()
+
+            return HttpResponse("Registrado")
