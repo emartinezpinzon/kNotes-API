245452f119359fb46603436238957c4229f9ce01
Emanuel Martínez <emartinezpinzon@gmail.com>
2017-12-09 23:15:35 -0500

    Creo los modelos etiqueta, nota y notaetiqueta

--- apps/nota/models.py
+++ apps/nota/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from apps.usuario.models import Autor
+
+class Etiqueta(models.Model):
+    nombre=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Nota(models.Model):
+    titulo=models.CharField(max_length=70)
+    contenido=models.TextField(null=False)
+    fecha=models.DateField(auto_now_add=True, null=False)
+    autor=models.ForeignKey(Autor)
+    disponbile=models.BooleanField()
+
+    def __str__(self):
+        return self.titulo
+
+class NotaEtiqueta(models.Model):
+	etiqueta=models.ForeignKey(Etiqueta)
+	nota=models.ForeignKey(Nota)
