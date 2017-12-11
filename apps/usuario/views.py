ca82889ce6067ebbac482dbf9028d6df9cf1954f
Emanuel Martínez <emartinezpinzon@gmail.com>
2017-12-10 21:33:09 -0500

    Importo HttpResponseRedirect

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
