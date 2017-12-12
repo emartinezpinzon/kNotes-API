67be8def0d051b04294f481277fffc60a32b2ddc
Emanuel Martínez <emartinezpinzon@gmail.com>
2017-12-11 23:51:56 -0500

    EtiquetaDetail OK

--- apps/nota/urls.py
+++ apps/nota/urls.py
@@ -3,4 +3,5 @@ from apps.nota import views
 
 urlpatterns = [
     url(r'^etiquetas/$', views.EtiquetaList.as_view()),
+    url(r'^etiquetas/(?P<pk>[0-9]+)/$', views.EtiquetaDetail.as_view()),
 ]
--- apps/nota/views.py
+++ apps/nota/views.py
@@ -27,4 +27,34 @@ class EtiquetaList(APIView):
         etiquetas = Etiqueta.objects.all()
         serializer = EtiquetaSerializer(etiquetas, many = True)
         
-        return Response(serializer.data, status=status.Http_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class EtiquetaDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Etiqueta.objects.get(pk=pk)
+        except Etiqueta.DoesNotExist:
+            raise Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk, format=None):
+        etiqueta = self.get_object(pk)
+        serializer = EtiquetaSerializer(Etiqueta)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        etiqueta = self.get_object(pk)
+        serializer = EtiquetaSerializer(etiqueta, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        etiqueta = self.get_object(pk)
+        etiqueta.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
