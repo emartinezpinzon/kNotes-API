9023d8930a6ce1b7b9dac1dc9ff0bd7c48e1da96
Emanuel Martínez <emartinezpinzon@gmail.com>
2017-12-17 15:58:18 -0500

    edito put

--- apps/nota/views.py
+++ apps/nota/views.py
@@ -116,7 +116,7 @@ class NotaDetail(APIView):
             
             nota.save()
             
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_200_OK)
         except Etiqueta.DoesNotExist:
             raise Response(status=status.HTTP_404_NOT_FOUND)
 
